a56f74ae67ea90ad19b6e1f8766aa3ec43c67a5a
maxueli <521266126@qq.com>
2023-10-26 13:31:21 +0800

    maidelong

---
+++ script/maidelong/index.js
@@ -0,0 +1,20 @@
+/*******************************
+脚本名称: 麦德龙会员
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/taurus.dmall.com\/mini\/program\/member\/getMemberCards url script-response-body https://raw.githubusercontent.com/maxueli/quanx/main/script/maidelong/index.js
+
+[mitm] 
+
+hostname = taurus.dmall.com
+
+*******************************/
+
+var body = $response.body;
+console.log($response);
+console.log(body);
+// .replace(/diamond\\":\d+/g,'diamond\\":999999')
+// .replace(/piece\\":\d+/g,'piece\\":888888')
+// $done(body);
